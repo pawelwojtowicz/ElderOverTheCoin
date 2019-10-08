398e0265d6b0575b8c097ccdb04bb16704adea65
Paweł Wójtowicz <elendil@konto.pl>
2019-10-08 21:39:44 +0200

    Final call for today

--- src/main/java/com/gutfilin/overthecoin/User/UserController.java
+++ src/main/java/com/gutfilin/overthecoin/User/UserController.java
@@ -1,6 +1,11 @@
 package com.gutfilin.overthecoin.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserController {
+	@Autowired
+	UserService service;
 
 }
